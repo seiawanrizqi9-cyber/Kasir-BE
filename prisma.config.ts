b1587a8a8680e0dc8082472199d28a686ec00640
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-02-18 10:10:19 +0700

    feat: change package.json

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/prisma/schema.prisma",
+  schema: "src/prisma",
+  migrations: {
+    path: "src/prisma/migrations",
+  },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
